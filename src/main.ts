2789cd23e76571e189fc84c38c881181b0a820e2
Alixan BALU <alixanb@orange.fr>
2025-09-25 19:01:55 +0200

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,9 @@
+import { loadImage } from "./core/Functions";
 import Vec2 from "./core/Vec2";
 import BlackHole from "./entities/Blackhole";
+import Ship from "./entities/Ship";
 import Canvas from "./systems/Canvas";
 import Galaxy from "./systems/Galaxy";
-import Ship from "./entities/Ship";
-
-const fps = 60;
-const interval = 1000 / fps;
-let lastTime = 0;
-
-function animate(time: number) {
-  requestAnimationFrame(animate);
-
-  const delta = time - lastTime;
-  if (delta >= interval) {
-    lastTime = time - (delta % interval);
-
-    galaxy.update();
-    galaxy.draw();
-  }
-}
-
-function loadImage(src: string): Promise<HTMLImageElement> {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => resolve(img);
-    img.onerror = (err) => reject(err);
-  });
-}
 
 const spriteThrusterOffUrl = "../public/ship.png";
 const spriteThrusterOnUrl = "../public/ship-thrust.png";
@@ -37,6 +13,20 @@ const gargantua = new BlackHole(new Vec2(0, 0), 50);
 let discovery = undefined;
 const canvas = new Canvas("#app");
 const galaxy = new Galaxy(canvas, [gargantua], discovery, 50000);
+
+const fps = 60;
+const interval = 1000 / fps;
+let lastTime = 0;
+
+export function animate(time: number) {
+  const delta = time - lastTime;
+  if (delta >= interval) {
+    lastTime = time - (delta % interval);
+
+    galaxy.update();
+    galaxy.draw();
+  }
+}
 
 Promise.all([loadImage(spriteThrusterOffUrl), loadImage(spriteThrusterOnUrl)])
   .then(([spriteThrusterOff, spriteThrusterOn]) => {

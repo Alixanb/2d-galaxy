9b0fa16dd778c920c417450c25ad5cae750b799f
Alixan BALU <alixan.tod@gmail.com>
2026-05-24 11:00:00 +0200

    feat: remove FloatingWindow + dead UI (SimulationHUD, ControlsPanel)

--- src/home.ts
+++
@@ -1,7 +0,0 @@
-import LandingPage from "./ui/LandingPage";
-
-const landing = new LandingPage();
-
-landing.onStart = (mode, showBlackholes) => {
-  window.location.href = `/simulation.html?mode=${mode}&bh=${showBlackholes}`;
-};
